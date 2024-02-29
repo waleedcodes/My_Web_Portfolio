cf0eafdfd6e3bf94bfbbd589572f6ece957c8fbd
WaleedCodes <110061477+waleedcodes@users.noreply.github.com>
2024-02-29 22:56:57 +0500

    some changes in Home Page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,6 +12,20 @@ const Footer = () => {
               Let's build something incredible together. Reach out via email or
               connect on LinkedIn and GitHub. Explore my portfolio for more.
             </p>
+            <ul>
+              <li>
+                <a href="">Home</a>
+              </li>
+              <li>
+                <a href="">About Us</a>
+              </li>
+              <li>
+                <a href="">Features</a>
+              </li>
+              <li>
+                <a href="">Testimonals</a>
+              </li>
+            </ul>
             <div className="ft_icon">
               <a
                 href="https://www.facebook.com/waleedcodes"
@@ -49,34 +63,6 @@ const Footer = () => {
                 <i className="fa-brands fa-linkedin"></i>
               </a>
             </div>
-          </div>
-          <div className="footer_2">
-            <h4 className="pb-2">Quick Links</h4>
-            <ul>
-              <li>
-                <a href="">Home</a>
-              </li>
-              <li>
-                <a href="">About Us</a>
-              </li>
-              <li>
-                <a href="">Features</a>
-              </li>
-              <li>
-                <a href="">Testimonals</a>
-              </li>
-            </ul>
-          </div>
-          <div className="footer_3">
-            <h4 className="pb-2">Leave A Message</h4>
-            <form action="/contact">
-              <div className="footer_3_input mt-4">
-                <input type="email" placeholder="Email Address" required />
-                <button className="btn3" type="submit">
-                  Send
-                </button>
-              </div>
-            </form>
           </div>
         </div>
       </div>
