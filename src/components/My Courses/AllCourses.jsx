0cede2fae283500828f962283f4f1996d48c7456
WaleedCodes <110061477+waleedcodes@users.noreply.github.com>
2024-03-02 23:29:44 +0500

    changes

--- src/components/My Courses/AllCourses.jsx
+++ src/components/My Courses/AllCourses.jsx
@@ -44,59 +44,6 @@ const AllCourses = ({ course }) => {
                 </p>
               </div>
             </div>
-            <div className="col-md-4">
-              <div className="about-col1" data-aos="fade-up">
-                <img src={img4} alt="" />
-                <h2>MERN Stack Development</h2>
-                <p>
-                  Building robust web applications with MongoDB, Express.js,
-                  React, and Node.js, ensuring seamless functionality and user
-                  experience.
-                </p>
-              </div>
-            </div>
-            <div className="col-md-4">
-              <div className="about-col1" data-aos="fade-down">
-                <img src={img5} alt="" />
-                <h2>Custom UI/UX Development</h2>
-                <p>
-                  Tailoring user interfaces and experiences to your brand and
-                  audience, making your website intuitive and enjoyable to
-                  navigate.
-                </p>
-              </div>
-            </div>
-            <div className="col-md-4">
-              <div className="about-col1" data-aos="fade-up">
-                <img src={img5} alt="" />
-                <h2>Custom Web Apps</h2>
-                <p>
-                  Designing and developing tailor-made web applications to
-                  address your specific needs and business goals.
-                </p>
-              </div>
-            </div>
-            <div className="col-md-4">
-              <div className="about-col1" data-aos="fade-down">
-                <img src={img5} alt="" />
-                <h2>PSD To HTML CSS JavaScript</h2>
-                <p>
-                  Effortlessly bring your PSD designs to life with our precise
-                  PSD to HTML/CSS/JavaScript conversion service. Achieve
-                  pixel-perfect, responsive web solutions.
-                </p>
-              </div>
-            </div>{" "}
-            <div className="col-md-4">
-              <div className="about-col1" data-aos="fade-up">
-                <img src={img5} alt="" />
-                <h2>Responsive Design</h2>
-                <p>
-                  Ensuring your digital assets look and function flawlessly on
-                  various devices, from desktops to smartphones.
-                </p>
-              </div>
-            </div>
           </div>
         </div>
       </div>
