41d3b1830bcef1e1177fba0d1d83ef3ba4baa92b
WaleedCodes <110061477+waleedcodes@users.noreply.github.com>
2024-03-02 23:39:08 +0500

    changes

--- src/components/My Courses/AllCourses.jsx
+++ src/components/My Courses/AllCourses.jsx
@@ -1,15 +1,21 @@
 const AllCourses = ({ course }) => {
   return (
     <>
-      <div className="">
+      {/* <div className="">
         <div className="col">
           <h5 className="text-white">{course.title}</h5>
           <p className="">{course.description}</p>
           <p className="">Instructor: {course.instructor}</p>
         </div>
-      </div>
+      </div> */}
       {/* ***************************** */}
-
+      <div className="">
+        <div className="about-col1" data-aos="fade-up">
+          <h5 className="text-white">{course.title}</h5>
+          <p className="">{course.description}</p>
+          <p className="">Instructor: {course.instructor}</p>
+        </div>
+      </div>
       {/* ***************************** */}
     </>
   );
