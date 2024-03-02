ca8782ae66878db4bb57cce14e0669fdf4c38470
WaleedCodes <110061477+waleedcodes@users.noreply.github.com>
2024-03-02 23:14:53 +0500

    changes

--- src/components/Courses.jsx
+++ src/components/Courses.jsx
@@ -1,7 +1,13 @@
-const Courses = () => {
-  return <div>
-    My Courses
-  </div>;
+const Courses = ({ course }) => {
+  return (
+    <div className="card mb-3">
+      <div className="card-body">
+        <h5 className="card-title">{course.title}</h5>
+        <p className="card-text">{course.description}</p>
+        <p className="card-text">Instructor: {course.instructor}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Courses;
