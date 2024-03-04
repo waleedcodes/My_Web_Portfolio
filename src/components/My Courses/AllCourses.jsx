a8cfc683cf382f754f21f697a8462b87136c8bcd
WaleedCodes <110061477+waleedcodes@users.noreply.github.com>
2024-03-04 23:42:46 +0500

    some changes

--- src/components/My Courses/AllCourses.jsx
+++ src/components/My Courses/AllCourses.jsx
@@ -19,6 +19,34 @@ const AllCourses = ({ course }) => {
           <div className="about-col1" data-aos="fade-up">
             <img src={course.src} alt="" className="img-fluid" />
             <h4 className="text-white pt-4">{course.title}</h4>
+            <ShowMoreText
+                      lines={5}
+                      more="Show more"
+                      less="Show less"
+                      anchorClass="show-more-less-clickable"
+                      onClick={executeOnClick}
+                      expanded={false}
+                      truncatedEndingComponent={"... "}
+                    >
+                      <p>
+                        Web development involves designing, building, and
+                        maintaining websites and web applications. In the early
+                        days, web development was simple, but now it has become
+                        complex with developers utilizing a wide range of
+                        programming languages, frameworks, and tools. There are
+                        three main components to web development: frontend
+                        development, backend development, and full-stack
+                        development. Frontend developers design and build the
+                        user interface using HTML, CSS, and JavaScript. Backend
+                        developers create the server-side components and use
+                        programming languages and databases to store and
+                        retrieve data. Full-stack developers build both the
+                        client-side and server-side components. Web development
+                        also involves other important considerations like
+                        accessibility, performance optimization, SEO, and
+                        security.
+                      </p>
+                    </ShowMoreText>
             <p className="">{course.description}</p>
             <h6 className="text-white">
               <b>Instructor:</b>
