b64a7a8f6ac95b918400b16b70b3ec0e238a55d3
WaleedCodes <110061477+waleedcodes@users.noreply.github.com>
2024-03-10 14:30:25 +0500

    some chanages

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import { ClerkProvider } from '@clerk/clerk-react';
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 if (!PUBLISHABLE_KEY) {
